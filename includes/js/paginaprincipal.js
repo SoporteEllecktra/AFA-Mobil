ac463e0e30c62ac19795be207b52a41da0b755d3
SoporteEllecktra <pablo@ellecktra.com>
2015-07-27 12:21:48 -0300

    Fix error process from registrarTelefono.js without net connection

--- includes/js/paginaprincipal.js
+++ includes/js/paginaprincipal.js
@@ -4,26 +4,7 @@ var porcentajeAbajo = 0.45;
 
 $(document).ready(function () {
 	MostrarDivBloqueo();
-    $.ajaxSetup({
-        error: function( jqXHR, textStatus, errorThrown ) {
-            if (jqXHR.status === 0) {
-                alert('Not connect: Verify Network.');
-            } else if (jqXHR.status == 404) {
-                alert('Requested page not found [404]');
-            } else if (jqXHR.status == 500) {
-                alert('Internal Server Error [500].');
-            } else if (textStatus === 'parsererror') {
-                alert('Requested JSON parse failed.');
-            } else if (textStatus === 'timeout') {
-                alert('Time out error.');
-            } else if (textStatus === 'abort') {
-                alert('Ajax request aborted.');
-            } else {
-                alert('Uncaught Error: ' + jqXHR.responseText);
-           }
-        }
-    });
-	
+
     swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         paginationClickable: true
